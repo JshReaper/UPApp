c8edb201188e09940c09fd5a8c2ec7f5fb4c85a3
Jacob Hansen <jshchansen@gmail.com>
2017-04-18 12:07:55 +0200

    added the calendar and made it show / hide depending on if you are admin or not

--- BookingSystem/BookingSystem/Form1.cs
+++ BookingSystem/BookingSystem/Form1.cs
@@ -12,9 +12,33 @@ namespace BookingSystem
 {
     public partial class Form1 : Form
     {
+        bool isAdmin;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void AdminCalendar_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void Loop_Tick(object sender, EventArgs e)
+        {
+            if (isAdmin)
+            {
+                AdminCalendar.Show();
+            }
+            else
+            {
+                AdminCalendar.Hide();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            isAdmin = true;
+            AdminCalendar.ShowWeekNumbers = true;
         }
     }
 }
